b85111c08bc859e072a1bd6283602e8a07442fb5
Vincent Hunter <jhearst29@gmail.com>
2023-08-15 20:51:08 -0400

    Initial Commit; Installed vecotr icons

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Text } from 'react-native';
+import colors from './src/theme/colors';
+import fonts from "./src/theme/fonts";
+import AntDesign from 'react-native-vector-icons/AntDesign';
+
+const App = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{ color: colors.primary, fontSize: fonts.size.lgSize }}>
+        Hello World
+        <AntDesign name='stepforward' size={25} />
+      </Text>
+    </View >
+  );
+};
+
+export default App;
---
+++ src/theme/fonts.ts
@@ -0,0 +1,20 @@
+const size = {
+    xsSize: 10,
+    smSize: 12,
+    primarySize: 14,
+    mdSize: 16,
+    lgSize: 20,
+    xlSize: 24,
+    xxlSize: 30
+
+};
+
+const weight = {
+    full: '900',
+    bold: 'bold',
+    normal: 'normal',
+    thin: '400',
+
+}
+
+export default { size, weight };
