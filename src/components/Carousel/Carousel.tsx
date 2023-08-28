8a3a28f679e4417bf1e8aeb33d2f60cbd80ebe5f
Vincent Hunter <jhearst29@gmail.com>
2023-08-27 22:44:41 -0400

    Began implementing State for Carousel Component; Began allowing component to map through each image file

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,4 +1,7 @@
 import { View, Image, FlatList } from 'react-native';
+import { useState } from 'react';
+import { useWindowDimensions } from 'react-native/types';
+import colors from '../../theme/colors';
 
 interface ICarousel {
     images: string[],
@@ -6,31 +9,52 @@ interface ICarousel {
 }
 
 const Carousel = ({ images }: ICarousel) => {
+    const { width } = useWindowDimensions();
+    const [activeImageIndex, setActiveImageIndex] = useState(0);
+
+    const viewabilityConfig = {
+        itemsVisiblePercentThreshold: 51,
+
+    }
+
+    const onViewableItemsChanged = ({ viewableItems }) => {
+        if (viewableItems > 0) {
+            setActiveImageIndex(viewableItems[0].index);
+        }
+    };
+
     return (
         <View>
             <FlatList
                 data={images}
                 renderItem={({ item }) => (
-                    <Image source={{ uri: item }} style={{ width: '100%', aspectRatio: 1 }} />
+                    <Image source={{ uri: item }}
+                        style={{ width: '100%', aspectRatio: 1 }} />
                 )}
                 horizontal={true}
                 pagingEnabled
+                onViewableItemsChanged={onViewableItemsChanged()}
             />
-            <View style={{ flexDirection: 'row' }}>
-                <View style={{
-                    width: 10,
-                    aspectRatio: 1,
-                    borderRadius: 5,
-                    backgroundColor: 'red'
-                }}
-                />
-
-                <View style={{
-                    width: 10,
-                    aspectRatio: 1,
-                    borderRadius: 5,
-                    backgroundColor: 'red'
-                }} />
+            <View style={{
+                flexDirection: 'row',
+                justifyContent: 'center',
+                position: 'absolute',
+                bottom: 0,
+                width: '100%',
+            }}>
+                {images.map((_, index) => (
+                    <View
+                        key={index}
+                        style={{
+                            width: 10,
+                            aspectRatio: 1,
+                            borderRadius: 5,
+                            backgroundColor: activeImageIndex === index ? colors.primary : colors.white,
+                            margin: 10,
+                            marginHorizontal: 5
+                        }}
+                    />
+                ))}
             </View>
         </View >
     )
